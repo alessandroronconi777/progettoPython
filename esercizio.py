0c70d0eb604299216afed5d46446af49c71e7dc3
AzureAD\AlessandroRonconi <alessandro.ronconi@itsrizzoli.it>
2025-11-07 13:45:08 +0100

    primo esercizio

---
+++ esercizio.py
@@ -0,0 +1,11 @@
+#commenti
+#addizione di due numeri interi
+addendo_uno, addendo_due = 5, 10
+
+def somma_due_numeri(num1, num2):
+    return num1 + num2
+
+somma = somma_due_numeri(addendo_uno, addendo_due)
+
+print("La somma di", addendo_uno, "e", addendo_due, "è:", somma)
+
